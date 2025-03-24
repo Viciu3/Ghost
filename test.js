e6b553f06969c96d066e8982323c59e1a9836022
АРТЁМ <kurichich5@gmail.com>
2025-03-24 13:30:33 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById('video1Button').addEventListener('click', function() {
+  window.location.href = 'video1.mp4';
+});
+
+document.getElementById('video2Button').addEventListener('click', function() {
+  window.location.href = 'video2.mp4';
+});
